4402c4081e078c26cb4815e8cfc35465df68eab5
CyanEbi <cyanebi@proton.me>
2024-09-22 23:05:01 +0200

    Basic modal dialog when pressing "Read more"

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,3 +9,18 @@ window.onload = function () {
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
     }
 };
+
+function readMore(postID) {
+    const post = document.getElementById(postID);
+
+    const dialog = document.getElementById('post_dialog');
+    dialog.innerHTML = `
+        <div class="post_content">
+            ${post.children[0].outerHTML}
+            ${post.children[1].children[0].outerHTML}
+            ${post.children[1].children[1].outerHTML}
+            <p>${post.children[1].children[2].innerHTML}</p>
+        </div>
+    `;
+    dialog.showModal();
+}
