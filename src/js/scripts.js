f191f591db26faa5314a0ffa7c0ba7592c690970
CyanEbi <cyanebi@proton.me>
2024-09-23 19:09:31 +0200

    Added features to modal dialog

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,11 @@
 window.onload = function () {
-
+    
+    const dialog = document.getElementById('post_dialog');
+    dialog.addEventListener('click', (event) => {
+        if (event.target === dialog) {
+            dialog.close();
+        }
+    });
     
 
     /* FUNCTIONS */
@@ -12,15 +18,27 @@ window.onload = function () {
 
 function readMore(postID) {
     const post = document.getElementById(postID);
+    const imageSrc = post.children[0].getAttribute('src');
 
     const dialog = document.getElementById('post_dialog');
     dialog.innerHTML = `
-        <div class="post_content">
-            ${post.children[0].outerHTML}
-            ${post.children[1].children[0].outerHTML}
-            ${post.children[1].children[1].outerHTML}
-            <p>${post.children[1].children[2].innerHTML}</p>
+        <div class="dialog_content">
+            <div class="dialog_banner">
+                <img src="${imageSrc}" class="banner_img">
+                <img src="assets/icons/close.svg" class="dialog_close" onclick="closeDialog()">
+            </div>
+            <div class="dialog_post">
+                ${post.children[1].children[0].outerHTML}
+                ${post.children[1].children[1].outerHTML}
+                <p>${post.children[1].children[2].innerHTML}</p>
+            </div>
+            </div>
+            
         </div>
     `;
     dialog.showModal();
+}
+
+function closeDialog() {
+    document.getElementById('post_dialog').close();
 }
