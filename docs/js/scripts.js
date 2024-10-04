4ad46b1ea310bd9ccbc9009b232ac9c59f1a2e0e
CyanEbi <cyanebi@proton.me>
2024-10-04 13:24:41 +0200

    Slider works on PC (except when resizing)

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,21 +1,36 @@
 var articleList;
-var slideNum = 0;
+
+const slides = document.getElementById('slides');
+const slideWidth = slides.children[0].offsetWidth;
+var slideIdx = 0;
 var totalSlides;
-var pageNum = 0;
+var currentSlideOffset;
+var isMouseDown = false;
+var dragInitX;
+
+var pageIdx = 0;
 var totalPages;
 
 window.onload = function () {
 
     fetch('posts.json')
         .then(response => response.json())
-        .then(json => {
-            articleList = json;
-            totalPages = Math.ceil(articleList.length/4);
-            document.getElementById('total_pages').innerText = `${totalPages}`;
-            loadPosts(0)
-        });
+            .then(json => {
+                articleList = json;
+                totalPages = Math.ceil(articleList.length/4);
+                document.getElementById('total_pages').innerText = `${totalPages}`;
+                loadPosts(0)
+            });
     
-    totalSlides = document.getElementsByClassName('slide').length;
+    
+    totalSlides = slides.children.length;
+    slides.append(slides.children[0].cloneNode(true));
+    slides.prepend(slides.children[totalSlides-1].cloneNode(true));
+    slides.addEventListener('mousedown', onDragStart);
+    document.addEventListener('mousemove', onDrag);
+    document.addEventListener('mouseup', onDragEnd);
+    slides.addEventListener('transitionend', onTransitionEnd);
+    jumpToSlide(0);
 
     const articles = document.getElementById('article_list').children;
     for (var i=0; i<articles.length; i++) {
@@ -40,16 +55,78 @@ window.onload = function () {
     }
 };
 
+function onDragStart(e) {
+    slides.classList.remove('transitioning');
+    e.preventDefault();
+
+    if (e.type == 'touchstart') {
+        
+    } else {
+        isMouseDown = true;
+        dragInitX = e.clientX;
+        slides.style.left = `${slides.offsetLeft}px`;
+    }
+}
+
+function onDrag(e) {
+    if (isMouseDown) {
+
+        // If user drags past the neighbouring slide
+        if (slides.offsetLeft > currentSlideOffset + slideWidth) {
+            isMouseDown = false;
+            switchSlide(-1);
+        } else if (slides.offsetLeft < currentSlideOffset - slideWidth) {
+            isMouseDown = false;
+            switchSlide(1);
+        } else {
+            slides.style.left = `${slides.offsetLeft + e.clientX - dragInitX}px`;
+            dragInitX = e.clientX;
+        }
+
+    }
+}
+
+function onDragEnd() {
+    if (isMouseDown) {
+        isMouseDown = false;
+    
+        if ((slides.offsetLeft - currentSlideOffset) > slideWidth/2) {
+            switchSlide(-1);
+        } else if ((slides.offsetLeft - currentSlideOffset) < -slideWidth/2) {
+            switchSlide(1);
+        } else {
+            switchSlide(0);
+        }
+    }
+}
+
+// -1 = previous slide (index), 0 = stay, 1 = next slide (index)
 function switchSlide(direction) {
-    if ((direction != -1) && (direction != 1)) {
-        return;
+    
+    // Avoid odd offsets by only switching slide if no transition is underway
+    if(!slides.classList.contains('transitioning'))
+    {
+        slides.classList.add('transitioning');
+        slideIdx += direction;
+        currentSlideOffset = -(slideIdx+1)*slideWidth;
+        slides.style.left = `${currentSlideOffset}px`;
+    }
+}
+
+function onTransitionEnd() {
+    slides.classList.remove('transitioning');
+    const moduloIdx = modulo(slideIdx, totalSlides);
+    if (slideIdx != moduloIdx) {
+        console.log(`Jump from ${slideIdx} to ${moduloIdx}`);
+        jumpToSlide(moduloIdx);
     }
     
-    const slides = document.getElementsByClassName('slide');
-    slides[slideNum].style.display = 'none';
+}
 
-    slideNum = modulo(slideNum + direction, totalSlides);
-    slides[slideNum].style.display = '';
+function jumpToSlide(idx) {    
+    slides.style.left = `${-(idx+1)*slideWidth}px`;
+    slideIdx = idx;
+    currentSlideOffset = slides.offsetLeft;
 }
 
 function modulo(dividend, divisor) {
